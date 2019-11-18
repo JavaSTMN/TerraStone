dfcbf5ce0d68163d66f7f0f8fae39eaaf3e65480
Océane Carpentier <oceane.carpentier13@gmail.com>
2019-11-18 14:27:06 +0100

    updated readme for javafx, play and exit game

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Découpage tâches :
 Clément -> Gestion Hero
 Nicolas -> Création Deck
 
+
+## Installation de JavaFX
+
+https://gluonhq.com/products/javafx/
+Télécharger le 13.0.1
+
+Sur Eclipse : 
+
+Window → Preferences → Java → Build Path → User Libraries → New
+Name : JavaFX13
+Add External Jars → Là où a été extrait le javaFX téléchargé
+→ lib → Sélectionner tous les jars
+
+File → New → Java Project → Choose name → Next → Libraries → Add Library → User Library → JavaFX13
--- TerraStone/src/application/Main.java
+++ TerraStone/src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
 	
 import java.io.IOException;
+import java.net.URL;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import view.MenuView;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -14,6 +16,7 @@ public class Main extends Application {
 	
 	private Stage mainStage;
 	private BorderPane mainContainer;
+	
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -26,7 +29,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();*/
 			
-			initialisationConteneurPrincipal();
+			initialisationConteneurPrincipal(getClass().getResource("../view/Menu.fxml"));
 			initialisationContenu();
 			
 		} catch(Exception e) {
@@ -34,10 +37,11 @@ public class Main extends Application {
 		}
 	}
 	
-	private void initialisationConteneurPrincipal() {
+	public void initialisationConteneurPrincipal(URL location) {
 
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("../view/Menu.fxml"));
+		loader.setLocation(location);
+		//loader.setLocation(getClass().getResource("../view/Menu.fxml"));
 		try {
 			//Le chargement nous donne notre conteneur
 			mainContainer = (BorderPane) loader.load();
@@ -57,17 +61,55 @@ public class Main extends Application {
 		loader.setLocation(getClass().getResource("../view/Menu.fxml"));
 		try {
 			//Nous récupérons notre conteneur qui contiendra les données
-			//Pour rappel, c'est un AnchorPane...
 			BorderPane conteneurPersonne = (BorderPane) loader.load();
 			//Qui nous ajoutons à notre conteneur principal
 			//Au centre, puisque'il s'agit d'un BorderPane
 			mainContainer.setCenter(conteneurPersonne);
+			MenuView menuCtrl = loader.getController();
+			System.out.println(this);
+			menuCtrl.setMainApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Launch the application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	/**
+	 * Get the main stage
+	 * @return the main stage
+	 */
+	public Stage getMainStage() {
+		return mainStage;
+	}
+
+	/**
+	 * Set the main stage
+	 * @param mainStage
+	 */
+	public void setMainStage(Stage mainStage) {
+		this.mainStage = mainStage;
+	}
+
+	/**
+	 * Get the main container
+	 * @return the main container
+	 */
+	public BorderPane getMainContainer() {
+		return mainContainer;
+	}
+
+	/**
+	 * Set the main containers
+	 * @param mainContainer
+	 */
+	public void setMainContainer(BorderPane mainContainer) {
+		this.mainContainer = mainContainer;
 	}
 }
